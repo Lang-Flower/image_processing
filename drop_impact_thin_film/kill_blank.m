919f0560f9e37216164afdd447a21e6bb77fea15
Lang-Flower <43107823+Lang-Flower@users.noreply.github.com>
2018-12-03 17:00:23 +0800

    version 1.0
    Mainly give some essential notes of key operations;
    Reinforce the comprehension about this function.

--- drop_impact_thin_film/kill_blank.m
+++ drop_impact_thin_film/kill_blank.m
@@ -1,7 +1,8 @@
 function [img_bwareaopen]=kill_blank(img_bw)
 %去掉小面积的多连通域部分
+%多余的联通区域包含液滴中的反光区域，液滴周围的图像杂点.
 se=strel('disk',3); %去掉连通域
 img_open=imopen(img_bw,se);
 img_bwareaopen_1=bwareaopen(img_open,2000);
-img_bwareaopen=1-bwareaopen(1-img_bwareaopen_1,10);
+img_bwareaopen=1-bwareaopen(1-img_bwareaopen_1,10);%对图像进行反向去掉液滴周围的图像噪点.
 end
