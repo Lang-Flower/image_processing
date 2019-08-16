36902c4470574d1ae9c4978be27b4a7402c0fb58
Lang-Flower <43107823+Lang-Flower@users.noreply.github.com>
2019-08-16 14:11:47 +0800

    Update velocity1.m

--- fit_bubble_diameter_under_drop/velocity1.m
+++ fit_bubble_diameter_under_drop/velocity1.m
@@ -3,10 +3,12 @@ clear all;
 close all;
 load('Velocity');
 figure('color',[1 1 1])
-plot(v_cal,'g+','Markersize',5)
+plot(v_cal,'rp','Markersize',10)
+axis([0 150 0.5 4.5])
+hold on
 errorbar(v_ex,err.*v_ex,'bd','Markersize',5,'Linewidth',1)
-yla=ylabel('velocity');
-set(yla,'Fontname','Times New Roman','FontWeight','Bold','FontSize',10)
-set(gca,'LineWidth',1.5) %ÉèÖÃ±ß¿ò
-% la=legend('experiment');
+yla=ylabel('Velocity');
+set(gca,'LineWidth',1.5) %è®¾ç½®è¾¹æ¡†
+la=legend('Calculate data','Experiment data');
+set([yla la],'Fontname','Times New Roman','FontWeight','Bold','FontSize',10)
 % set(la,'box','off')
